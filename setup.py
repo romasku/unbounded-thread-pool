4329ee6b808a94523a78f0de82ab176baf48038b
Roman Skurikhin <roman.skurikhin@cruxlab.com>
2020-05-20 15:06:57 +0300

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='unbounded-thread-pool',
-    version='0.1.0',
+    version='0.2.0',
     packages=['unbounded_thread_pool'],
     license='MIT',
     description='ThreadPool with unlimited workers count.',
